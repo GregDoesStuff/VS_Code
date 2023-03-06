2d12ae63d6c553408bcd830cad777c8d0e8d1c7f
Greg_Does_Stuff <110251922+GregDoesStuff@users.noreply.github.com>
2023-03-05 17:43:38 -0800

    Update pwd2.py
    I added two while loops that allow the user to input values up to 5 times, and I also defined a MAX_TRIES variable to make it easy to change the maximum number of tries allowed.

--- pwd2.py
+++ pwd2.py
@@ -3,24 +3,38 @@ import random
 import string
 import datetime
 
+MAX_TRIES = 5
+
 # Function to generate a password of specified length
 def generate_password(length):
     password = ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(length))
     return password
 
 # Prompt user for number of passwords to generate
-try:
-    num_passwords = int(input("How many passwords do you need at this time?: "))
-except ValueError:
-    print("Please enter a valid number for the number of passwords.")
-    exit()
+tries = 0
+while tries < MAX_TRIES:
+    try:
+        num_passwords = int(input("How many passwords do you need at this time?: "))
+        break
+    except ValueError:
+        tries += 1
+        print("Please enter a valid number for the number of passwords.")
+        if tries == MAX_TRIES:
+            print(f"Max tries ({MAX_TRIES}) exceeded. Exiting.")
+            exit()
 
 # Prompt user for length of password
-try:
-    length = int(input("How many characters do you need your password(s) to be?: "))
-except ValueError:
-    print("Please enter a valid number for the length of the password.")
-    exit()
+tries = 0
+while tries < MAX_TRIES:
+    try:
+        length = int(input("How many characters do you need your password(s) to be?: "))
+        break
+    except ValueError:
+        tries += 1
+        print("Please enter a valid number for the length of the password.")
+        if tries == MAX_TRIES:
+            print(f"Max tries ({MAX_TRIES}) exceeded. Exiting.")
+            exit()
 
 # Get the current date and time
 now = datetime.datetime.now()
@@ -38,4 +52,5 @@ with open("passwords.txt", "w") as file:
 # Print a message indicating that the passwords are ready
 print("Thank you! Your passwords are ready and have been saved to passwords.txt")
     
-#33/5/23 - Added shebang for Linux
+#33/5/23 - I added two while loops that allow the user to input values up to 5 times,
+# and I also defined a MAX_TRIES variable to make it easy to change the maximum number of tries allowed.
