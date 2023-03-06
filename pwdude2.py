3e4b213d25bbaa802d1cc0b72dd8a49dbee776d4
Greg_Does_Stuff <110251922+GregDoesStuff@users.noreply.github.com>
2023-03-05 16:05:36 -0800

    Update pwdude2.py

--- pwdude2.py
+++ pwdude2.py
@@ -28,3 +28,13 @@ with open("passwords.txt", "w") as file:
 
 # Print a message indicating that the passwords are ready
 print("Thank you! Your passwords are ready and have been saved to passwords.txt")
+
+except ValueError:
+    print("Please enter a valid number for the number of passwords and length")
+except Exception as e:
+    print("An unexpected error occurred: ", e)
+    
+#3/5/23 - In this modified code, we have added try-except blocks to catch two types of errors:
+#ValueError: This exception will be raised if the user inputs an invalid value for the number of passwords or the length of the password. If this exception is caught, the code will print a message asking the user to enter a valid input.
+#Exception: This exception will catch any other unexpected errors that may occur during the execution of the code. If this exception is caught, the code will print a message indicating that an unexpected error has occurred and display the error message.
+#By adding these try-except blocks, we can improve the robustness and reliability of the code by handling errors gracefully and preventing the code from crashing due to invalid inputs or unexpected errors.
