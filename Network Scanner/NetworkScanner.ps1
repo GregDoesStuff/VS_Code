3c9cd095e8e31fa01a0b583ca362dacbb13c0890
Greg_Does_Stuff <110251922+GregDoesStuff@users.noreply.github.com>
2023-05-03 18:53:33 -0700

    fix progress bar for port scanns

--- Network Scanner/NetworkScanner.ps1
+++ Network Scanner/NetworkScanner.ps1
@@ -6,10 +6,8 @@ $end_ip = Read-Host "Enter the end IP address"
 $total_ips = [int]($end_ip.Substring($end_ip.LastIndexOf(".") + 1)) - [int]($start_ip.Substring($start_ip.LastIndexOf(".") + 1)) + 1
 $total_ports = 100
 
-# Declare an array to store the up IP addresses
-$up_ips = @()
-
 # Loop through the IP addresses and ping each one
+$up_ips = @()
 for ($i = [int]($start_ip.Substring($start_ip.LastIndexOf(".") + 1)); $i -le [int]($end_ip.Substring($end_ip.LastIndexOf(".") + 1)); $i++) {
     $ip_address = $start_ip.Substring(0, $start_ip.LastIndexOf(".") + 1) + $i.ToString()
     if (Test-Connection -ComputerName $ip_address -Count 1 -Quiet) {
@@ -19,14 +17,12 @@ for ($i = [int]($start_ip.Substring($start_ip.LastIndexOf(".") + 1)); $i -le [in
     
     # Update the IP scan progress bar
     $ip_progress = [int](($i - [int]($start_ip.Substring($start_ip.LastIndexOf(".") + 1)) + 1) * 100 / $total_ips)
-    Write-Progress -Activity "Scanning IP addresses" -PercentComplete $ip_progress -Status "Scanning IP address $ip_address" -CurrentOperation "Scanning IP address $ip_address" 
+    Write-Progress -Activity "Scanning IP addresses and ports" -PercentComplete $ip_progress -Status "Scanning IP address $ip_address" -CurrentOperation "Scanning IP address $ip_address" 
 }
 
-# Loop through the up IP addresses and scan the ports
+# Loop through the up IPs and scan ports
 foreach ($ip in $up_ips) {
-    Write-Host "Results for $($ip):"
-    
-    # Loop through ports 1-100 and test each one
+    Write-Host "Scanning ports for $ip ..."
     $ports = 1..100
     $total_ports = $ports.Count
     $count = 0
@@ -39,10 +35,15 @@ foreach ($ip in $up_ips) {
         elseif ($result.UdpTestSucceeded) {
             Write-Host "Port $port is listening on $ip"
         }
-
+        
         # Update the progress bar for port scanning
         $port_progress = [int]($count * 100 / $total_ports)
         $ip_progress = [int](($up_ips.IndexOf($ip) + 1) * 100 / $up_ips.Count)
-        Write-Progress -Activity "Scanning ports on up IP addresses" -PercentComplete $ip_progress -Status "Scanning port $port on IP address $ip" -CurrentOperation "Testing port $port on IP address $ip" -PercentComplete $port_progress
+        Write-Progress -Activity "Scanning IP addresses and ports" -PercentComplete $ip_progress -Status "Scanning ports for $ip" -CurrentOperation "Testing port $port on IP address $ip" -PercentComplete $port_progress
     }
+    
+    # Write port scan results for the current IP address to file
+    Write-Host "Results for $ip:"
+    Get-NetTCPConnection -ComputerName $ip -State Listen | Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort | Sort-Object LocalPort | Format-Table -AutoSize
+    Write-Host ""
 }
